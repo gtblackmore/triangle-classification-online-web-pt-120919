0583e01f6bfde453cbdf7dfd9c23576dd1f430e7
George Blackmore <gtblackmore@gmail.com>
2020-01-18 15:55:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,21 +8,21 @@ class Triangle
   end
 
   def kind
-    if self.valid
+    if @a <= 0 || @b <= 0 || @c <= 0
       raise TriangleError
-    elsif @a == @b && @b == @c
+    end
+    
+    if (@a + @b) > @c || (@a + @c) > @b || (@b + @c) > @a
+      raise TriangleError
+    end
+    
+    if @a == @b && @b == @c
       :equilateral
     elsif @a == @b || @b == @c || @a == @c
       :isosceles
     else
       :scalene
     end
-  end
-
-  def valid
-    side_length = @a <= 0 || @b <= 0 || @c <= 0
-    length_sum = (a + b) > c || (a + c) > b || (b + c) > a
-    side_length || length_sum
   end
 
   class TriangleError < StandardError
