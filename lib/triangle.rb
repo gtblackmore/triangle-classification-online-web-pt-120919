0dcad00841bdae65b363b485cd56234fb064e71b
George Blackmore <gtblackmore@gmail.com>
2020-01-18 15:51:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  
+   def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if self.valid
+      raise TriangleError
+    elsif @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @b == @c || @a == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid
+    side_length = @a <= 0 || @b <= 0 || @c <= 0
+    length_sum = (a + b) > c || (a + c) > b || (b + c) > a
+    side_length || length_sum
+  end
+
+  class TriangleError < StandardError
+  end
+  
+  
 end
